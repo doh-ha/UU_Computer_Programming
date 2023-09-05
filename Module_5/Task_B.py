beb5c70e7728d9e5cf03e47d5b95f41bc2e27390
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-09-05 17:59:46 +0200

    close 출력 성공 못 함

--- Module_5/Task_B.py
+++ Module_5/Task_B.py
@@ -30,18 +30,28 @@ def rectangle(x, y, width, height, color):
 
 
 def move_random(t):
+    t.speed(0)
     t.forward(random.randrange(0, 25))
-    t.left(random.randrange(-45, 45))
+    angle = t.heading()
+    t.left(random.randrange(angle-45, angle+45))
+
+
+def print_close():
+    turtle.hideturtle()
+    turtle.color("green")
+    turtle.write("Hello")
 
 
 rectangle(-200, -200, 500, 500, "lightblue")
+
 t1 = make_turtle(random.randrange(-200, 300),
                  random.randrange(-200, 300), 'red')
 t2 = make_turtle(random.randrange(-200, 300),
                  random.randrange(-200, 300), 'green')
-for i in range(250):
+for i in range(500):
     move_random(t1)
     move_random(t2)
-
+    # if abs(t1.x-t2.x) <= 50 | abs(t1.y-t2.y) <= 50:
+    #     print_close()
 
 turtle.done()
