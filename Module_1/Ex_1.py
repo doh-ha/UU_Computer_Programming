9de5c6510d7659eddb82b49a90dbb3fb3f0effe7
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-09-05 16:59:33 +0200

    T1_A_line done

---
+++ Module_1/Ex_1.py
@@ -0,0 +1,4 @@
+
+f = float(input("Enter Fahrenheit"))
+c = 5/9*(f-32)
+print("Corresponding Celcius is", c)
---
+++ Module_5/Task_1.py
@@ -0,0 +1,65 @@
+import turtle
+
+
+def make_turtle(x, y):
+    t = turtle.Turtle()
+    jump(t, x, y)
+    return t
+
+
+def rectangle(x, y, width, height, color):
+    t = make_turtle(x, y)
+    t.hideturtle()
+    t.fillcolor(color)
+    t.begin_fill()
+    for dist in [width, height, width, height]:
+        t.speed(0)
+        t.forward(dist)
+        t.left(90)
+    t.end_fill()
+
+
+def tricolore(x, y, h):
+    w = h/2  # färgfältens bredd
+    rectangle(x, y, w, h, 'blue')
+    rectangle(x+w, y, w, h, 'white')
+    rectangle(x+2*w, y, w, h, 'red')
+
+
+def pentagram(x, y, side):
+    t = make_turtle(x, y)
+    t.hideturtle()
+    t.setheading(270 - 36/2)
+    for i in range(5):
+        t.speed(0)
+        t.forward(side)
+        t.left(180-36)
+        t.fillcolor('green')
+        t.begin_fill()
+
+
+def jump(t, x, y):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+
+
+tricolore(-100, -100, 200)
+
+t = turtle.Turtle()
+t.hideturtle()
+
+
+for i in range(10):
+
+    if i < 5:
+        t.fillcolor('green')
+        t.begin_fill()
+        pentagram(-150 + i*100, -150, 100)
+    else:
+        t.fillcolor('green')
+        t.begin_fill()
+        pentagram(-650 + i*100, 250, 100)
+t.end_fill()
+
+turtle.done()
