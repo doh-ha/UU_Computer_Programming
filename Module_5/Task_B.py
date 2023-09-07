bceda685f74d3ff6ebd825b1b972a8f88b4ba6b3
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-09-07 10:48:02 +0200

    잘 움직임

--- Module_5/Task_B.py
+++ Module_5/Task_B.py
@@ -36,22 +36,37 @@ def move_random(t):
     t.left(random.randrange(angle-45, angle+45))
 
 
-def print_close():
-    turtle.hideturtle()
-    turtle.color("green")
-    turtle.write("Hello")
-
+# def print_close():
+#     # turtle.hideturtle()
+#     turtle.color("green")
+#     turtle.write("Close")
 
 rectangle(-200, -200, 500, 500, "lightblue")
 
-t1 = make_turtle(random.randrange(-200, 300),
-                 random.randrange(-200, 300), 'red')
-t2 = make_turtle(random.randrange(-200, 300),
-                 random.randrange(-200, 300), 'green')
+t1_x = random.randrange(-200, 300)
+t1_y = random.randrange(-200, 300)
+t2_x = random.randrange(-200, 300)
+t2_y = random.randrange(-200, 300)
+
+
+t1 = make_turtle(t1_x,
+                 t1_y, 'red')
+t2 = make_turtle(t2_x,
+                 t2_y, 'green')
+
+
+count = 0
 for i in range(500):
     move_random(t1)
     move_random(t2)
-    # if abs(t1.x-t2.x) <= 50 | abs(t1.y-t2.y) <= 50:
-    #     print_close()
 
+    x_diff = t1_x-t2_x
+    y_diff = t1_y-t2_y
+    if abs(x_diff) <= 50 and abs(y_diff) <= 50:
+
+        t1.write("Close")
+        count += 1
+
+
+print('count is', count)
 turtle.done()
