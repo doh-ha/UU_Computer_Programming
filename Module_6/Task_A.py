64b24c05b420149a71fdce42aee79146a4bd95ad
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-09-13 16:21:47 +0200

    Module6_TaskA 완성

--- Module_6/Task_A.py
+++ Module_6/Task_A.py
@@ -1,44 +1,65 @@
-def smooth_a(a, n):
-    # extend left side
+def extend(x, n):
     r = [0 for i in range(len(x)+n)]
 
     # add original list
-    r[n:] = a
+    r[n:] = x
 
     # extend right side
     for i in range(n):
         r.append(0)
 
+    return r
+
+
+def smooth_a(x, n):
+    r = extend(x, n)
+
     # extend the value of the side
     for i in range(n):
-        r[i] = a[0]  # left
-        r[-i-1] = a[-1]  # right
+        r[i] = x[0]  # left
+        r[-i-1] = x[-1]  # right
 
     new = r.copy()  # copy
 
     # calculate the value
-    for i in range(len(a)):
+    for i in range(len(x)):
         sum = r[i+n]  # itslef
         for j in range(n):
             sum += r[i+n-(j+1)]
-            # print("index is ", i+n-(j+1))
             sum += r[i+n+(j+1)]
-
-        # print("sum is ", sum)
         new[i+n] = sum/(2*n+1)
-        # print("r is ", r)
-        # print("new is ", new)
 
     new = new[n:len(r)-n]  # slice to the original size
     return new
 
 
-# def smooth_b(a, n):
-#     print(n)
+def smooth_b(x, n):
+
+    r = extend(x, n)
+    new = r.copy()  # copy
+    # print(r)
+    # calculate the value
+    for i in range(len(x)):
+        sum = r[i+n]  # itslef
+        num = 1
+        for j in range(n):
+            sum += r[i+n-(j+1)]  # left
+
+            if (i+n-(j+1) >= n):
+                num += 1
+            sum += r[i+n+(j+1)]  # right
+            if (i+n+(j+1) <= len(r)-n-1):
+                num += 1
+
+        new[i+n] = sum/num
+
+    new = new[n:len(r)-n]  # slice to the original size
+    return new
 
 
+# main
 x = [1, 2, 6, 4, 5, 0, 1, 2]
 print('smooth_a(x, 1): ', smooth_a(x, 1))
 print('smooth_a(x, 2): ', smooth_a(x, 2))
-# print('smooth_b(x, 1): ', smooth_b(x, 1))
-# print('smooth_b(x, 2): ', smooth_b(x, 2))
+print('smooth_b(x, 1): ', smooth_b(x, 1))
+print('smooth_b(x, 2): ', smooth_b(x, 2))
